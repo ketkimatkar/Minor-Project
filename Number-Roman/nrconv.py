6e9bc93e6ae42eb0859a230fffa25d578f00b5ec
Ketki Matkar <matkarketki12345@gmail.com>
2020-06-23 15:09:35 +0530

    Final Commit

--- Number-Roman/nrconv.py
+++
@@ -1,7 +0,0 @@
-choice=int(input("Enter you choice for conversion:\n 1-Number to Roman \n 2-Roman to Number"))
-if choice==1:
-    import num_roman                #num_roman.py will be executed
-elif choice==2:
-    import roman_num                #roman_num.py will be executed
-else:
-    print("You have entered wrong choice")
